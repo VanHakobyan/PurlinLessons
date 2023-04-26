26468c18acffa29db5a40938c27b098bfc33d8b4
VanikHakobyan <van_h@purlin.com>
2023-04-26 10:58:48 +0400

    minor changes

--- StageOne/Lesson1/Program.cs
+++ StageOne/Lesson1/Program.cs
@@ -10,16 +10,25 @@ Console.WriteLine("Hello, World!");
 //Console.WriteLine(result);
 //Console.WriteLine(rem);
 
-Console.WriteLine("Please input number");//4329/10
-string text = Console.ReadLine();
-int input = int.Parse(text);
-int n1 = input % 10;//9
-input = input / 10;//432
-int n2 = input % 10;//2
-input = input / 10;//43
-int n3 = input % 10;//3
-input = input / 10;//4
-int n4 = input % 10;//4
+var input = 1589;
+//'1','5','8','9'
+//"1"
+//[1,5,8,9]
 
-int result = n1 + n2 + n3 + n4;
+var result  = input.ToString().ToCharArray().Select(x=>int.Parse(x.ToString())).Sum();
 Console.WriteLine(result);
+//Console.WriteLine("Please input number");//4329/10
+//string text = Console.ReadLine();
+//int input = int.Parse(text);
+//int n1 = input % 10;//9
+//input = input / 10;//432
+//int n2 = input % 10;//2
+//input = input / 10;//43
+//int n3 = input % 10;//3
+//input = input / 10;//4
+//int n4 = input % 10;//4
+
+//int result = n1 + n2 + n3 + n4;
+//Console.WriteLine(result);
+
+////4325, "4325"
